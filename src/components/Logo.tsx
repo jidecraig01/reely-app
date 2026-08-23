478c689ad17c90c4ce1f4d831546c8f49c2dda61
jidecraig01 <jidecraig01@gmail.com>
2026-08-23 04:09:58 +0100

    Update logo image references

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ export default function Logo({ size = 40, className = '', onClick }: LogoProps) 
   return (
     <div className={`flex items-center ${className}`} onClick={onClick}>
       <img
-        src="/magnific_isolated-logo-featuring-t_iGYrE7N3uK.png"
+        src="/generated-1787454506378-nodjs.png"
         alt="REELY"
         width={Math.round(size * 1.5)}
         height={size}
